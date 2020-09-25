3334e0dd1362255af7eb70926febc7da5c2bd965
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-24 20:29:36 -0700

    Add functionality for Spec 4

--- app.rb
+++ app.rb
@@ -7,10 +7,10 @@ root_name = gets.chomp
 new_directory = Project.new(root_name)
 new_directory.make_directories
 
-# Include Script File?
+# Create Script and Gemfile
 puts "Include a script file? (y/n)"
 script_response = gets.chomp
-new_directory.include_script(script_response)
+new_directory.make_root_files(script_response)
 
 # Create Subdirectories and Files
 puts "Class name:"
--- lib/project.rb
+++ lib/project.rb
@@ -9,14 +9,14 @@ class Project
     Dir.mkdir("../#{@root_name}/spec")
   end
 
-  def include_script(script_response)
+  def make_root_files(script_response)
+    File.write("../#{@root_name}/Gemfile", "source 'https://rubygems.org'\n\ngem 'rspec'\ngem 'pry'")
     script_response.downcase == 'y' ? File.write("../#{@root_name}/app.rb", "") : return
   end
 
   def make_files(class_name)
-    File.write("../#{@root_name}/Gemfile", "source 'https://rubygems.org'\ngem 'rspec'\ngem 'pry'")
-    File.write("../#{@root_name}/lib/#{class_name}.rb", "")
-    File.write("../#{@root_name}/spec/#{class_name}_spec.rb", "")
+    File.write("../#{@root_name}/lib/#{class_name}.rb", "class #{class_name.capitalize}\n\nend")
+    File.write("../#{@root_name}/spec/#{class_name}_spec.rb", "require ('rspec')\nrequire ('#{class_name}')\n\ndescribe('#{class_name.capitalize}') do\n\nend")
   end
 
   
