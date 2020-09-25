1d469a51152bfa303842307658a5e07275d98830
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-24 20:12:29 -0700

    Add functionality for one class to be added

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 #!usr/bin/ruby
 require ('./lib/project')
 
+# Create New Root Directory
 puts "Enter new root directory name"
 root_name = gets.chomp
 new_directory = Project.new(root_name)
 new_directory.make_directories
-new_directory.make_files
+
+# Include Script File?
+puts "Include a script file? (y/n)"
+script_response = gets.chomp
+new_directory.include_script(script_response)
+
+# Create Subdirectories and Files
+puts "Class name:"
+class_name = gets.chomp
+new_directory.make_files(class_name)
+
+
 
--- lib/project.rb
+++ lib/project.rb
@@ -9,8 +9,16 @@ class Project
     Dir.mkdir("../#{@root_name}/spec")
   end
 
-  def make_files
-    File.write("../#{@root_name}/Gemfile", "source 'https://rubygems.org'\ngem 'rspec'\ngem 'pry'")
+  def include_script(script_response)
+    script_response.downcase == 'y' ? File.write("../#{@root_name}/app.rb", "") : return
   end
+
+  def make_files(class_name)
+    File.write("../#{@root_name}/Gemfile", "source 'https://rubygems.org'\ngem 'rspec'\ngem 'pry'")
+    File.write("../#{@root_name}/lib/#{class_name}.rb", "")
+    File.write("../#{@root_name}/spec/#{class_name}_spec.rb", "")
+  end
+
+  
 
 end
